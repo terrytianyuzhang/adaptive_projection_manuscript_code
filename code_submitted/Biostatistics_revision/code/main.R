14b3a8de239b6967b5e3981c92ee68918530f863
Tianyu Zhang 张天宇 <zty119950705@gmail.com>
2026-09-09 16:31:23 -0700

    before adding the new simulatino section

--- code_submitted/Biostatistics_revision/code/main.R
+++ code_submitted/Biostatistics_revision/code/main.R
@@ -4,16 +4,16 @@
 # src/031_mean_comparison_simulation_config.R
 
 # find proper covariance matrices for the simulation
-file.path(".", "001_find_sparse_covariance_matrix.R") |> source()
+file.path(".", "001_find_sparse_covariance_matrix.R")
 
 # Run the simulation with 16 cores.
 # bash 035_run_complete_mean_comparison_pipeline.sh 16
 
 # Create type-I and power plot using the output from 035_
-file.path(".", "036_plot_mean_comparison_results.R") |> source()
+file.path(".", "036_plot_mean_comparison_results.R") 
 
 # Create publciation plot
-file.path(".", "037_")
+file.path(".", "037_publication_plot.R")
 
 
 
